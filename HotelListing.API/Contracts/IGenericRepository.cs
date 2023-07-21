f56640d0419f7eeccaf4af5fd7f79c1c081d85ed
suzannah.thompson <suzannah.thompson@xdesign.com>
2023-07-21 11:44:19 +0100

    Remove error

--- HotelListing.API/Contracts/IGenericRepository.cs
+++ HotelListing.API/Contracts/IGenericRepository.cs
@@ -4,7 +4,7 @@ namespace HotelListing.API.Contracts;
 
 public interface IGenericRepository<T> where T : class
 {
-    Task<T> GetAsync(int id);
+    Task<T> GetAsync(int? id);
     Task<List<T>> GetAllAsync();
     Task<T> AddAsync(T entity);
     Task Deletesync(int id);
